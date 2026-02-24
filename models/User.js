cb3fd44daec7c678d0e359e9704910531cd59c87
love gahe <lavkumargahe@gmail.com>
2026-02-24 14:31:07 +0530

    resetPassword code done

--- models/User.js
+++ models/User.js
@@ -41,12 +41,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-//   token: {
-//     type: String,
-//   },
-//   resetPasswordExpires: {
-//     type: Date,
-//   },
+  token: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
   courseProgress: [
     {
       type: mongoose.Schema.Types.ObjectId,
